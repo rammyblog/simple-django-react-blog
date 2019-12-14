ded7a7a48055a95cfd7ccc840d2b1cfdae05d6b7
Onasanya Tunde <Onasanyatunde67@gmail.com>
2019-12-14 02:33:40 +0100

    Added the list and retrieve serializers

---
+++ articles/api/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from articles.models import Article
+from .serializers import ArticleSerializer
+
+class ArticleListView(ListAPIView):
+    queryset =  Article.objects.all()
+    serializer_class = ArticleSerializer
+
+class ArticleDetailView(RetrieveAPIView):
+    queryset =  Article.objects.all()
+    serializer_class = ArticleSerializer
